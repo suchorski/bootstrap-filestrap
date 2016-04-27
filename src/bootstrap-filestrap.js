def912b7f5af6b939c3b95eab87773b3fec78e10
Thiago Suchorski <suchorski@ymail.com>
2016-04-26 21:56:51 -0300

    Update bootstrap-filestrap.js

--- src/bootstrap-filestrap.js
+++ src/bootstrap-filestrap.js
@@ -1,4 +1,4 @@
-/*! FileStrap v1.0.0 | (c) 3S Suchorski | Created with Bootstrap v3.3.6 and jQuery 1.12.3 */
+/*! FileStrap v1.0.1 | (c) 3S Suchorski | Created with Bootstrap v3.3.6 and jQuery 1.12.3 */
 (function($) {
 	'use strict';	
 	$.fn.filestrap = function(option, value) {
@@ -12,29 +12,31 @@
 			$(this).parents().eq(2).children('[data-fs-id="inputFilename"]').attr('placeholder', value);
 			break;
 		default:
-			var options = $.extend({}, $.fn.filestrap.defaults, option);
-			return this.each(function() {
-				var $div = $('<div>', { class: 'input-group' });
-				var $textInput = $('<input>', { type: 'text', id: $(this).data('id'), class: 'form-control', placeholder: $(this).attr('placeholder') }).prop('readonly', true).attr('data-fs-id', 'inputFilename');
-				var $groupSpan = $('<span>', { class: 'input-group-btn' });
-				var $label = $('<label>', { class: 'btn btn-primary', 'for': $(this).attr('id') });
-				var $fileInput = $('<input>', { type: 'file', id: $(this).attr('id') }).hide().attr('data-fs-id', 'inputFile').prop('filestrap', true);
-				var $fileSpan = $('<span>', { class: 'glyphicon glyphicon-folder-open' });
-				if (options.hasTrash) {
-					var trashButton = $('<button>', { class: 'btn btn-danger' }).attr('data-fs-id', 'buttonTrash').click(function() {
-						$(this).parent().find('label input[data-fs-id="inputFile"]').filestrap('clear');
-					}).hide();
-					var trashSpan = $('<span>', { class: 'glyphicon glyphicon-trash' }).html(' ' + options.trashText);
-					$groupSpan.append(trashButton.append(trashSpan));
-				}
-				$fileInput.change(function() {
-					$(this).parents().eq(2).children('[data-fs-id="inputFilename"]').val($(this).val());
+			if (!$(this).prop('filestrap')) {				
+				var options = $.extend({}, $.fn.filestrap.defaults, option);
+				return this.each(function() {
+					var $div = $('<div>', { class: 'input-group' });
+					var $textInput = $('<input>', { type: 'text', id: $(this).data('id'), class: 'form-control', placeholder: $(this).attr('placeholder') }).prop('readonly', true).attr('data-fs-id', 'inputFilename');
+					var $groupSpan = $('<span>', { class: 'input-group-btn' });
+					var $label = $('<label>', { class: 'btn btn-primary', 'for': $(this).attr('id') });
+					var $fileInput = $('<input>', { type: 'file', id: $(this).attr('id') }).hide().attr('data-fs-id', 'inputFile').prop('filestrap', true);
+					var $fileSpan = $('<span>', { class: 'glyphicon glyphicon-folder-open' });
 					if (options.hasTrash) {
-						$(this).parents().eq(1).children('[data-fs-id="buttonTrash"]').show();
+						var trashButton = $('<button>', { class: 'btn btn-danger' }).attr('data-fs-id', 'buttonTrash').click(function() {
+							$(this).parent().find('label input[data-fs-id="inputFile"]').filestrap('clear');
+						}).hide();
+						var trashSpan = $('<span>', { class: 'glyphicon glyphicon-trash' }).html(' ' + options.trashText);
+						$groupSpan.append(trashButton.append(trashSpan));
 					}
-				}).prop('disabled', $(this).is('[disabled]'));
-				$(this).replaceWith($div.append($textInput).append($groupSpan.append($label.append($fileInput).append($fileSpan.html(' ' + options.browserText)))));
-			});
+					$fileInput.change(function() {
+						$(this).parents().eq(2).children('[data-fs-id="inputFilename"]').val($(this).val());
+						if (options.hasTrash) {
+							$(this).parents().eq(1).children('[data-fs-id="buttonTrash"]').show();
+						}
+					}).prop('disabled', $(this).is('[disabled]'));
+					$(this).replaceWith($div.append($textInput).append($groupSpan.append($label.append($fileInput).append($fileSpan.html(' ' + options.browserText)))));
+				});
+			}
 		}
 	};
 	$.fn.filestrap.defaults = {
